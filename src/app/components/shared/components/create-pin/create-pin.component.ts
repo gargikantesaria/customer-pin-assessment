f692530eb8158929d9ed91ec8b4dee0c48d99003
KANTESAG <gargi.kantesaria@contractors.roche.com>
2023-09-28 13:43:04 +0530

    [fix] Add pin design and functionality

--- src/app/components/shared/components/create-pin/create-pin.component.ts
+++ src/app/components/shared/components/create-pin/create-pin.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FileUploader } from 'ng2-file-upload';
 @Component({
   selector: 'app-create-pin',
   templateUrl: './create-pin.component.html',
   styleUrls: ['./create-pin.component.css']
 })
-export class CreatePinComponent {
+export class CreatePinComponent implements OnInit {
 
+  pinForm: FormGroup;
+
+  // for image upload
+  uploader: FileUploader = new FileUploader({
+    url: '',
+    itemAlias: 'image'
+  });
+  public hasBaseDropZoneOver: boolean = false;
+  uploadedImage: { name: string, url: string } | undefined;
+
+  collaboratorList: string[] = [];
+  displayCollaboratorError: boolean = false;
+
+  constructor(public activeModal: NgbActiveModal) {
+    this.pinForm = new FormGroup({
+      'title': new FormControl('', Validators.required),
+      'collaborators': new FormControl([], Validators.required),
+      'privacy': new FormControl('public', Validators.required)
+    });
+  }
+
+  ngOnInit(): void {
+    // get customer list from localstorage
+    const getCustomers = localStorage.getItem('customerList');
+    if (getCustomers) {
+      this.collaboratorList = JSON.parse(getCustomers).map(customer => customer['title']);
+    }
+  }
+
+  // close modal
+  closeModal() {
+    this.activeModal.close('closePin');
+    this.pinForm.reset();
+    this.uploadedImage = null;
+  }
+
+  // Trigger the hidden file input by clicking it
+  openFileUploader() {
+    const fileInput = document.querySelector('.image-upload-input') as HTMLInputElement;
+    if (fileInput) {
+      fileInput.click();
+    }
+  }
+
+  // drag-drop file
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  // select file
+  public onFileSelected(event: File[]) {
+    const file: File = event[0];
+    this.uploadedImage = {
+      name: file.name,
+      url: URL.createObjectURL(file)
+    };
+  }
+
+  // on blur select input : display validation error message accordingly
+  onBlurSelectInput() {
+    this.pinForm.get('collaborators').invalid ? this.displayCollaboratorError = true : '';
+  }
+
+
+  // save pin
+  savePin() {
+    let combinedPinList = [];
+    const existingPinList = localStorage.getItem('pinList');
+
+    existingPinList ? combinedPinList = JSON.parse(existingPinList) : '';
+
+    let addedPinData = this.pinForm.value;
+    addedPinData['image'] = this.uploadedImage;
+
+    combinedPinList.push(addedPinData);
+    localStorage.setItem('pinList', JSON.stringify(combinedPinList));
+
+    this.closeModal();
+  }
 }
--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { CreateCustomerComponent } from './components/create-customer/create-cus
 import { CreatePinComponent } from './components/create-pin/create-pin.component';
 import { NgxSelectModule } from 'ngx-select-ex';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NgxSelectModule,
     FormsModule,
     ReactiveFormsModule,
+    FileUploadModule
   ],
   exports: [
     CreateCustomerComponent,
